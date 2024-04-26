a9a2d705b875ea7d2a048d151ed09157b0894daf
Jesimiel <jesimielbarbosa@gmail.com>
2024-04-26 02:25:55 -0300

    Criando recursos para obter dados e imprimir em tela

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+         // Criando um objeto Scanner para ler entrada do usuário
+         Scanner scanner = new Scanner(System.in);
+        
+         // Solicitando e lendo o número da agência
+         System.out.println("Por favor, digite o número da Agência com o dígito: ");
+         String agencia = scanner.nextLine();
+         
+         // Solicitando e lendo o número da conta
+         System.out.println("Agora o número da Conta: ");
+         int numeroConta = Integer.parseInt(scanner.nextLine());
+         
+         // Solicitando e lendo o nome e sobrenome do cliente
+         System.out.println("Digite o nome e o sobrenome do Cliente: ");
+         String nomeCliente = scanner.nextLine();
+         
+         // Solicitando e lendo o saldo da conta
+         System.out.println("E o Saldo em conta: ");
+         String saldoConta = scanner.nextLine(); // Lê a entrada como uma string
+         double saldoContaCliente = Double.parseDouble(saldoConta); // Converte para double
+         
+         // Exibindo uma mensagem de boas-vindas ao cliente com as informações fornecidas
+         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " + 
+                 "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " +
+                 saldoContaCliente + " já está disponível para saque. ");
+         
+         // Fechando o objeto Scanner para liberar recursos
+         scanner.close();
+
     }
 }
